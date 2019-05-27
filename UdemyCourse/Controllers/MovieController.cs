180d7d2e0da1e6c9d047f23680d72dcf9597c77b
alucard1792 <dz9217@hotmail.com>
2019-05-26 20:53:07 -0500

    Add project files.

---
+++ UdemyCourse/Controllers/MovieController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UdemyCourse.Models;
+
+namespace UdemyCourse.Controllers
+{
+    public class MovieController : Controller
+    {
+        // GET: Movie
+        public ActionResult Random()
+        {
+            Movie movie = new Movie() {Name = "Matrix"};
+            return View(movie);
+        }
+    }
+}
