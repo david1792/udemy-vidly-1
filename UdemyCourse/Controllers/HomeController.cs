7d729018153dfd5bf34d492bcb3d87e3c42749fe
alucard1792 <dz9217@hotmail.com>
2019-05-26 20:55:50 -0500

    6. Adding a Theme.mp4

--- UdemyCourse/Controllers/HomeController.cs
+++ UdemyCourse/Controllers/HomeController.cs
@@ -21,7 +21,7 @@ namespace UdemyCourse.Controllers
         }
 
         public ActionResult Contact()
-        {
+        { 
             ViewBag.Message = "Your contact page.";
 
             return View();
