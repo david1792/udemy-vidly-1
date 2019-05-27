f4b8a1d0789ed3ce469d090cbb683fe3c0725670
alucard1792 <dz9217@hotmail.com>
2019-05-26 21:11:42 -0500

    part 2 - 2. Action Results.mp4

--- UdemyCourse/Controllers/MovieController.cs
+++ UdemyCourse/Controllers/MovieController.cs
@@ -13,7 +13,18 @@ namespace UdemyCourse.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie() {Name = "Matrix"};
-            return View(movie);
+            //return View(movie);
+            //return Content("hello world);
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new {page = 1, sortBy="name" });
+            /*
+             * This is the most used return types in controllers
+             * Content return a stream
+             * httpNotFound return a 404 http status
+             * empty return a empty page
+             * redirectToAction return a redirection to specific controller, third argument is a get parameter response
+             */
         }
     }
 }
