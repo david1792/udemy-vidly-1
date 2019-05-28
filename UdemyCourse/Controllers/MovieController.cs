1d0128cf25b0f1c2307bf493587b5f7c180a0f6d
alucard1792 <dz9217@hotmail.com>
2019-05-27 20:58:55 -0500

    part 2 - 7. View Models.mp4 & 8. Razor Syntax.mp4 In this case we used helper entities(Entidades de apoyo) to render two models in one view and we use atributes with validations to render a dynamic class,

--- UdemyCourse/Controllers/MovieController.cs
+++ UdemyCourse/Controllers/MovieController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using UdemyCourse.Models;
+using UdemyCourse.ViewModels;
 
 namespace UdemyCourse.Controllers
 {
@@ -13,20 +14,17 @@ namespace UdemyCourse.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie() {Name = "Matrix"};
-
-            //other ways to send data to a view: view data dictionary but this way is fragile because if the view doesn't exists this string we get a null pointer reference and in the view we need to cast becouse  view data is a dynamic type
-            ViewData["Movie"] = movie;
-            //this way is better becouse is added in runtime which mean we don't get  compile time safety, but anyway,
-            //the better form is pass into a return method 
-            ViewBag.Movie = movie;
-            var viewResult = new ViewResult();
-            viewResult.ViewData.Model = movie;
-            /*
-             * so, where go the model when i return a view result?
-             * var viewResult = new ViewResult();
-             * viewResult.ViewData.Model = movie; -> the model is save in the Model property 
-             */
-            return View(movie);
+            var customers = new List<Customer>()
+            {
+                new Customer{ Name = "Customer 1"},
+                new Customer{Name = "Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel()
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
         }
 
     }
