40bde2b11b7dbcd116054e5346184ecfb783fb33
alucard1792 <dz9217@hotmail.com>
2019-05-27 20:20:42 -0500

    part 2 - 6. Passing Data to Views.mp4

--- UdemyCourse/Controllers/MovieController.cs
+++ UdemyCourse/Controllers/MovieController.cs
@@ -13,51 +13,21 @@ namespace UdemyCourse.Controllers
         public ActionResult Random()
         {
             Movie movie = new Movie() {Name = "Matrix"};
-            //return View(movie);
-            //return Content("hello world);
-            //return HttpNotFound();
-            //return new EmptyResult();
-            return RedirectToAction("Index", "Home", new {page = 1, sortBy="name" });
+
+            //other ways to send data to a view: view data dictionary but this way is fragile because if the view doesn't exists this string we get a null pointer reference and in the view we need to cast becouse  view data is a dynamic type
+            ViewData["Movie"] = movie;
+            //this way is better becouse is added in runtime which mean we don't get  compile time safety, but anyway,
+            //the better form is pass into a return method 
+            ViewBag.Movie = movie;
+            var viewResult = new ViewResult();
+            viewResult.ViewData.Model = movie;
             /*
-             * This is the most used return types in controllers
-             * Content return a stream
-             * httpNotFound return a 404 http status
-             * empty return a empty page
-             * redirectToAction return a redirection to specific controller, third argument is a get parameter response
+             * so, where go the model when i return a view result?
+             * var viewResult = new ViewResult();
+             * viewResult.ViewData.Model = movie; -> the model is save in the Model property 
              */
+            return View(movie);
         }
 
-        public ActionResult Edit(int id)
-        {
-            /*
-             *if we open the route config, we see the default route,
-             * url: "{controller}/{action}/{id}", the default route spected a parameter call id
-             * if we change the name of this method, mvs response with an error e.g.
-             *ActionResult Edit(int movieId)
-             */
-            //to test embebled parameter: https://localhost:44339/Movie/edit/1
-            //to test query string parameter: https://localhost:44339/Movie/edit?id=1
-            return Content("id=" + id);
-        }
-
-        public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            //to test: https://localhost:44339/Movie?pageIndex=2&sortBy=release
-            if (!pageIndex.HasValue)
-                pageIndex = 0;
-            if (string.IsNullOrEmpty(sortBy))
-                sortBy = "name";
-            return Content(string.Format("pageIndex={0} sortBy={1}", pageIndex, sortBy));
-        }
-        //atribute route, more powerful and we can specify constrains
-        [Route("movie/released/{year:regex(\\d{4})}/{month:range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, int month)
-        {
-            /*
-             * bad because missing parameters url :https://localhost:44339/movies/released
-             * good url https://localhost:44339/movie/released/2015/02
-             */
-            return Content(year + "/" + month);
-        }
     }
 }
