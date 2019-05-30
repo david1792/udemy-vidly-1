25f8ff0a4ae3d97d76fedb39486d6cc260196d50
alucard1792 <dz9217@hotmail.com>
2019-05-29 21:08:41 -0500

    part 2 - 12. Solution.mp4

--- UdemyCourse/Controllers/CustomersController.cs
+++ UdemyCourse/Controllers/CustomersController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using UdemyCourse.Models;
 
 namespace UdemyCourse.Controllers
 {
     public class CustomersController : Controller
     {
+        List<Customer> customers = new List<Customer>()
+        {
+            new Customer{ Name = "Customer 1", Id = 1},
+            new Customer{Name = "Customer 2", Id = 2}
+        };
         // GET: Customers
         public ActionResult Index()
         {
-            return View();
+            int random = new Random().Next(2);
+            System.Diagnostics.Debug.WriteLine(random);
+            if (random == 0)
+            {
+               customers.Clear();
+            }
+            return View(customers);
+        }
+        public ActionResult ShowDetails(int id)
+        {
+            return View(customers.Where(i => i.Id == id).FirstOrDefault());
         }
     }
 }
