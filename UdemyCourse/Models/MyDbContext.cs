2777f09287c7f43b9caeabf4679575e345b90d7f
alucard1792 <dz9217@hotmail.com>
2019-06-04 21:19:25 -0500

    part 2 - latest commit

---
+++ UdemyCourse/Models/MyDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace UdemyCourse.Models
+{
+    public class MyDbContext:DbContext
+    {
+        public MyDbContext()
+        {
+        }
+    }
+}
